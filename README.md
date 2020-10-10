cbd8f9248c7c00d8a61634b28ff4b12b9d56e7c1
danyakitik <kitik955@gmail.com>
2020-10-10 15:19:19 +0300

    commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+
+## Telegram Bot
+
+
+https://t.me/hillel_dz_17_kitik_bot
+
+##launch bot 
+in php-fpm container write 
+
+php artisan bot:update
---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Order::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'order_id'=>$this->faker->unique()->randomNumber(),
+            'title'=>$this->faker->domainName,
+            'status'=>rand(1,3)
+        ];
+    }
+}
